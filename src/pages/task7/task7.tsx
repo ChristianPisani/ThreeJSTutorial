81c878c2404d41f4bdcaedc73fe5b5fc47709c0e
Christian Pisani Thorvik <christian96-th@hotmail.com>
2024-01-22 10:12:46 +0100

    post processing

--- src/pages/task7/task7.tsx
+++ src/pages/task7/task7.tsx
@@ -3,18 +3,35 @@ import { Suspense, useEffect, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import Loader from '../../components/Loader.tsx'
 import {
+    BoxGeometry,
     Color,
     Group,
     InstancedMesh,
+    Mesh,
+    MeshBasicMaterial,
     MeshPhysicalMaterial,
     Object3D,
     SphereGeometry,
+    Vector2,
     Vector3,
 } from 'three'
 import { OrbitControls, Sky, Stats, useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 import { MeshSurfaceSampler } from 'three/addons/math/MeshSurfaceSampler.js'
-import { useBeforeUnload } from 'react-router-dom'
+import {
+    Bloom,
+    EffectComposer,
+    Glitch,
+    GodRays,
+    Noise,
+    Vignette,
+} from '@react-three/postprocessing'
+import {
+    BlendFunction,
+    GlitchMode,
+    KernelSize,
+    Resolution,
+} from 'postprocessing'
 
 const LogoModelSimpleApproach = () => {
     const ref = useRef<Group>(null!)
@@ -60,12 +77,13 @@ const LogoModel = () => {
     const sampler = new MeshSurfaceSampler(logo.scene.children[0]).build()
     const position = new THREE.Vector3()
 
-    const geometry = new SphereGeometry()
-    geometry.scale(0.005, 0.005, 0.005)
+    const geometry = new BoxGeometry()
+    geometry.scale(0.01, 0.01, 0.01)
     const material = new MeshPhysicalMaterial()
-    material.roughness = 0.1
+    material.roughness = 0.2
     material.color = new Color('blue')
-    material.metalness = 1
+    material.metalness = 0
+    material.emissive = new Color('lightblue')
     const tempObject = new Object3D()
 
     const particles = []
@@ -103,32 +121,76 @@ const LogoModel = () => {
     })
 
     return (
-        <group ref={ref} position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
-            <instancedMesh
-                ref={instanceRef}
-                args={[geometry, material, numberOfParticles]}
-            ></instancedMesh>
-        </group>
+        <>
+            <group
+                ref={ref}
+                position={[0, 0, 0]}
+                rotation={[Math.PI / 2, 0, 0]}
+            >
+                <instancedMesh
+                    ref={instanceRef}
+                    args={[geometry, material, numberOfParticles]}
+                ></instancedMesh>
+            </group>
+            {/*<group ref={ref}>
+                <primitive object={logo.scene}></primitive>
+            </group>*/}
+            <EffectComposer>
+                <GodRays
+                    sun={instanceRef}
+                    blendFunction={BlendFunction.SCREEN}
+                    samples={40}
+                    density={0.99}
+                    decay={0.99}
+                    weight={0.75}
+                    exposure={0.05}
+                    clampMax={1}
+                    width={Resolution.AUTO_SIZE}
+                    height={Resolution.AUTO_SIZE}
+                    kernelSize={KernelSize.LARGE}
+                    blur={true}
+                />
+                <Noise opacity={0.025} />
+                <Vignette darkness={0.75} />
+                <Bloom
+                    luminanceThreshold={0}
+                    luminanceSmoothing={0.9}
+                    height={200}
+                    opacity={1}
+                />
+                <Glitch
+                    mode={GlitchMode.SPORADIC}
+                    delay={new Vector2(0.5, 1)}
+                    duration={new Vector2(0.5, 0.75)}
+                />
+            </EffectComposer>
+        </>
     )
 }
 
 const Task7Canvas = () => {
+    const sunMesh = new Mesh(
+        new SphereGeometry(0.1),
+        new MeshBasicMaterial({
+            color: '#CC8C39',
+            transparent: true,
+            opacity: 1,
+        })
+    )
+
     return (
-        <>
-            <Canvas
-                camera={{
-                    position: [0, 0, 3],
-                }}
-            >
-                <Suspense fallback={<Loader />}>
-                    <OrbitControls />
-                    <Sky />
-                    <ambientLight intensity={4} />
-                    <directionalLight intensity={1} position={[0, 0, 10]} />
-                    <LogoModel />
-                </Suspense>
-            </Canvas>
-        </>
+        <Canvas
+            camera={{
+                position: [0, 0, 3],
+            }}
+        >
+            <Suspense fallback={<Loader />}>
+                <OrbitControls />
+                <ambientLight intensity={4} />
+                <directionalLight intensity={1} position={[0, 0, 10]} />
+                <LogoModel />
+            </Suspense>
+        </Canvas>
     )
 }
 
